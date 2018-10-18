7fe6def25415cdd61bbaa133eb550671695c6747
Lurix6 <vasyapopovich23@gmail.com>
2018-10-18 13:43:37 +0300

    Updates

---
+++ precache-manifest.41af755795e05f240f019745f953e07d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mysite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3ded49a2979a7fef5d5b",
+    "url": "/mysite/static/js/runtime~main.3ded49a2.js"
+  },
+  {
+    "revision": "08488c90668c677c3e07",
+    "url": "/mysite/static/js/main.08488c90.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/mysite/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "08488c90668c677c3e07",
+    "url": "/mysite/static/css/main.f6e56626.chunk.css"
+  },
+  {
+    "revision": "e8f0adfb66f5d7b64c313b0adf5dd090",
+    "url": "/mysite/index.html"
+  }
+];
